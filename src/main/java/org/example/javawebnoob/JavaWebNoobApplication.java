402730b7310c349f1d6efda3297e4d041e92d1f2
ssgsg <986843038@qq.com>
2024-09-08 17:08:56 +0800

    搭建好三层架构的基本框架。

---
+++ src/main/java/org/example/javawebnoob/JavaWebNoobApplication.java
@@ -0,0 +1,13 @@
+package org.example.javawebnoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebNoobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebNoobApplication.class, args);
+    }
+
+}
