0e02197151be0d3919f63352dc00e70ce0616ad0
ssgsg <986843038@qq.com>
2024-09-10 17:30:44 +0800

    集成jwt令牌。

--- src/main/java/org/example/javawebnoob/controller/LoginController.java
+++ src/main/java/org/example/javawebnoob/controller/LoginController.java
@@ -4,11 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.javawebnoob.pojo.Emp;
 import org.example.javawebnoob.pojo.Result;
 import org.example.javawebnoob.service.EmpService;
+import org.example.javawebnoob.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -21,6 +25,16 @@ public class LoginController {
     public Result login(@RequestBody Emp emp) {
         log.info("员工登录: {}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+        if (e != null) {
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+
+            String token = JwtUtils.generateJwt(claims);
+            return Result.success(token);
+        }
+        return Result.error("用户名或密码错误");
     }
 }
--- src/test/java/org/example/javawebnoob/JavaWebNoobApplicationTests.java
+++ src/test/java/org/example/javawebnoob/JavaWebNoobApplicationTests.java
@@ -1,11 +1,17 @@
 package org.example.javawebnoob;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-@SpringBootTest
+//@SpringBootTest
+
 class JavaWebNoobApplicationTests {
 
     @Test
@@ -20,4 +26,27 @@ class JavaWebNoobApplicationTests {
         }
     }
 
+    @Test
+    public void testGenJwt() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1);
+        claims.put("username", "admin");
+
+        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256,
+                        "ssgJwre05swrtLtSe01c0CkIe051tLtsllIsgJ2031sgJwre051tLtSe01c0CkIllI203Se01c0CkIllI203")//签名算法和密钥
+                .setClaims(claims)//自定义内容（载荷）
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))//设置有效期为1小时
+                .compact();
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void testParseJwt() {
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsImV4cCI6MTcyNTk1ODUwMX0" +
+                ".Fk1Ot3Fj5sU4qFkPKlMzHQhRKF78ljAaEGKPz85XsrA";
+        Claims claims = Jwts.parser().setSigningKey(
+                "ssgJwre05swrtLtSe01c0CkIe051tLtsllIsgJ2031sgJwre051tLtSe01c0CkIllI203Se01c0CkIllI203").parseClaimsJws(jwt).getBody();
+        System.out.println(claims);
+    }
 }
+
